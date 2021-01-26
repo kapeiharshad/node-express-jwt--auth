f79c3d2814703af9695377a722c948bd87f3aba2
kapeiharshad <kapeiharshad@gmail.com>
2021-01-26 23:46:52 +0530

    logout added

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -7,5 +7,6 @@ router.get("/signup", authService.signup_get);
 router.post("/signup", authService.signup_post);
 router.get("/login", authService.login_get);
 router.post("/login", authService.login_post);
+router.get("/logout", authService.logout_get);
 
 module.exports = router;
