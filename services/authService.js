279c99aeb4075a62cfd764faec5d2bbf792edf57
kapeiharshad <kapeiharshad@gmail.com>
2021-01-26 19:05:40 +0530

    login api and validation done

--- services/authService.js
+++ services/authService.js
@@ -4,12 +4,20 @@ const jwt = require("jsonwebtoken");
 // handle errors
 const handleErrors = (err) => {
   // console.log("err", err);
-  console.log("----------", err.message, err.code);
   let errors = { email: "", password: "" };
 
   // duplicate email error
   if (err.code === 11000) {
     errors.email = "That email is already registered";
+    return errors;
+  }
+
+  if (
+    err.message === "incorrect email" ||
+    err.message === "incorrect password"
+  ) {
+    errors.email = "That email or password is incorrect";
+    errors.password = "That email or password is incorrect";
     return errors;
   }
 
@@ -19,6 +27,7 @@ const handleErrors = (err) => {
       errors[properties.path] = properties.message;
     });
   }
+
   return errors;
 };
 
@@ -56,7 +65,13 @@ module.exports.signup_post = async (req, res) => {
 
 module.exports.login_post = async (req, res) => {
   const { email, password } = req.body;
-
-  console.log(email, password);
-  res.send("user login");
+  try {
+    const user = await User.login(email, password);
+    const token = createToken(user._id);
+    res.cookie("jwt", token, { httpOnly: true, maxAge: maxAge * 1000 });
+    res.status(201).json({ user: user._id });
+  } catch (error) {
+    const errors = handleErrors(error);
+    res.status(400).json({ errors });
+  }
 };
